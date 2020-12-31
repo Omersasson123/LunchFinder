e1b7b7ce27cfe5a57bd165e2dafe0ea7ee91f028
Keshav Gupta <kgupta@csumb.edu>
2020-12-30 20:25:41 -0800

    intial commit

---
+++ test.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+boostrap = Bootstrap(app)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
